e2e88d68f8e66e4f4aec0d9f40112145fb8c64ac
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-25 14:18:07 +0000

    edit signup file type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
   
-  get "/signup" do 
-    erb :signup
-  end 
+  # get "/signup" do 
+  #   erb :signup
+  # end 
   
-  post "/signup" do 
-   if params[:username] == "" || params[:password] == ""
-      redirect '/signup'
-    else
-      User.create(username: params[:username], password: params[:password])
-      redirect '/login'
-    end
-  end
+  # post "/signup" do 
+  # if params[:username] == "" || params[:password] == ""
+  #     redirect '/signup'
+  #   else
+  #     User.create(username: params[:username], password: params[:password])
+  #     redirect '/login'
+  #   end
+  # end
 
 end 
