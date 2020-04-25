90f0138c5cd925d44ba2d0feb7ad585444af44f5
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-25 17:41:47 +0000

    user model has_secure_password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   get "/signup" do 
-    erb :signup
+    erb :"users/signup"
   end 
   
   post "/signup" do 
@@ -10,6 +10,21 @@ class UsersController < ApplicationController
     else
       User.create(username: params[:username], password: params[:password])
       redirect '/login'
+    end
+  end
+  
+  get "/login" do 
+    erb :login
+  end
+  
+  post "/login" do 
+    user = User.find_by(:username => params[:username])
+  
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id 
+        redirect "/account"
+    else 
+        redirect "failure" 
     end
   end
 
