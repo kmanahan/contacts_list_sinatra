0f822df5e54b258bd9d795efcdbc36425e77fcfe
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-30 17:50:35 +0000

    if successful will edit a contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,4 +7,18 @@ class ContactsController < ApplicationController
     @contact = current_user.contacts.create(params)
     erb :"contacts/show"
   end 
+  
+  get "/contacts/:id/edit" do
+    @edit = Contact.find_by_id(params[:id])
+    erb :"contacts/edit"
+  end 
+  
+  post "/contacts/:id/edit" do 
+    redirect "/contacts/:id/edit"
+  end
+  
+  patch "contacts/:id" do 
+    @edit.update
+    erb:"contacts/show"
+  end 
 end 
