01431aca9057fae6e917a860b482cfbe924926f4
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-05-01 18:51:51 +0000

    validates username to be unique

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,3 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
-  validates :username, uniqueness: true, presence: true
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base 
   has_many :contacts
   has_secure_password
+  validates :username, uniqueness: true, presence: true
 end 
